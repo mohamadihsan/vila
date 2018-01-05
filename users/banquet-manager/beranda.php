9b9e398df3c46360ffce4095fed0327bb928ee13
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-01-05 17:26:15 +0700

    update beranda

--- users/banquet-manager/beranda.php
+++ users/banquet-manager/beranda.php
@@ -15,6 +15,50 @@
             <div class="row">
                 <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
                     <!-- PAGE CONTENT BEGINS -->
+                    <h5><b>Filter :</b></h5>
+                    <form action="" method="get">
+                        <select name="id" class="form-control select2" required>
+                            <?php
+                            // retrieve data dari API
+                            $file = file_get_contents($url_api."tampilkan_data_bahan_makanan.php");
+                            $json = json_decode($file, true);
+                            $i=0;
+                            while ($i < count($json['data'])) {
+                                $id_bahan_makanan[$i] = $json['data'][$i]['id_bahan_makanan'];
+                                $nama_bahan_makanan[$i] = $json['data'][$i]['id_bahan_makanan'].' - '.$json['data'][$i]['nama_bahan_makanan'];
+                                ?>
+                                <option value="<?= $id_bahan_makanan[$i] ?>" <?php if(isset($_GET['id'])){ if($_GET['id']==$id_bahan_makanan[$i]) echo 'selected'; } ?>> <?= $nama_bahan_makanan[$i] ?></option>
+                                <?php
+                                $i++;
+                            }
+                            ?>
+                        </select>
+                        <select name="periode" class="form-control select2" required>
+                            <?php
+                            // retrieve data dari API
+                            $file = file_get_contents($url_api."tampilkan_data_tahun_peramalan.php");
+                            $json = json_decode($file, true);
+                            $i=0;
+                            if (count($json['data']) == 0) {
+                                ?><option value="<?= date('Y')?>"><?= date('Y') ?></option><?php
+                            }else{
+                                while ($i < count($json['data'])) {
+                                    $tahun[$i] = $json['data'][$i]['tahun'];
+                                    ?>
+                                    <option value="<?= $tahun[$i] ?>" <?php if(isset($_GET['periode'])){ if($_GET['periode']==$tahun[$i]) echo 'selected'; } ?>> <?= $tahun[$i] ?></option>
+                                    <?php
+                                    if ($i==count($json['data'])-1) {
+                                        ?><option value="<?= $tahun[$i]+1 ?>" <?php if(isset($_GET['periode'])){ if($_GET['periode']==$tahun[$i]+1) echo 'selected'; } ?>> <?= $tahun[$i]+1 ?></option><?php
+                                    }
+
+                                    $i++;
+                                }
+                            }
+                            ?>
+
+                        </select>
+                        <button type="submit" class="btn btn-sm">Filter</button>
+                    </form>
 
                     <div style="width:100%;">
                         <canvas id="canvas"></canvas>
