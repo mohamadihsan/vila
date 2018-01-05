7a6c53ee10ad8e94e151c452c9e09da8b8048724
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-01-05 13:32:18 +0700

    fix bug monitoring persediaan stok

--- action/tampilkan_data_monitoring_stok.php
+++ action/tampilkan_data_monitoring_stok.php
@@ -4,32 +4,41 @@ require_once '../config/connection.php';
 
 // inisialisasi
 $periode    = date('m-Y');
+$sql = "SELECT DATE_FORMAT(tanggal, '%m-%Y') as periode FROM persediaan_bahan_makanan ORDER BY tanggal DESC LIMIT 1";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$periode = $row['periode'];
 
 // sql statement
 $sql = "SELECT
-        	x.id_bahan_makanan,
-            x.nama_bahan_makanan,
-        	y.sisa
+            x.id_bahan_makanan,
+            ( x.barang_masuk - x.barang_keluar ) AS sisa,
+            x.nama_bahan_makanan
         FROM
-        	(
-        		SELECT
-                    nama_bahan_makanan,
-        			id_bahan_makanan
-        		FROM
-        			bahan_makanan
-        	) AS x
-        LEFT JOIN (
-        	SELECT
-        		id_bahan_makanan,
-        		SUM(sisa) AS sisa
-        	FROM
-        		persediaan_bahan_makanan
-        	WHERE
-        		sisa = 0
-        	AND DATE_FORMAT(tanggal, '%m-%Y') = '$periode'
-        	GROUP BY
-        		1
-        ) AS y ON x.id_bahan_makanan = y.id_bahan_makanan";
+            (
+                SELECT
+                    p.id_bahan_makanan,
+                    SUM( p.barang_masuk ) AS barang_masuk,
+                    SUM( p.barang_keluar ) AS barang_keluar,
+                    p.harga_satuan,
+                    DATE_FORMAT( p.tanggal, '%m-%Y' ) AS tanggal,
+                    bm.nama_bahan_makanan,
+                    bm.satuan
+                FROM
+                    persediaan_bahan_makanan p
+                    LEFT JOIN bahan_makanan bm ON bm.id_bahan_makanan = p.id_bahan_makanan
+
+                WHERE
+                    DATE_FORMAT( p.tanggal, '%m-%Y' ) = '$periode'
+                GROUP BY
+                    1,
+                    4,
+                    5
+                ORDER BY
+                    p.id_bahan_makanan ASC,
+                    tanggal DESC
+            ) AS x
+        WHERE ( x.barang_masuk - x.barang_keluar ) = 0";
 $result = mysqli_query($conn, $sql);
 $data = array();
 while ($row = mysqli_fetch_assoc($result)) {
